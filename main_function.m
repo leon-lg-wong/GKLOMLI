12e95b28d26f4decde8ace18965af336dcdf638d
Leon Wong <3947349+leon-lg-wong@users.noreply.github.com>
2022-11-03 01:31:39 +0800

    Update main_function.m

--- main_function.m
+++ main_function.m
@@ -1,10 +1,10 @@
-clear
+% clear
 tic
-load Function_data/lncrna_function_similairty_matrix.mat
-load Function_data/mirna_function_similairty_matrix.mat
-load Function_data/invalid_lnc_function.mat
-load Function_data/invalid_mi_function.mat
-load /Dataset/miRNA-lnRNA_interaction/interaction.mat
+load ./Dataset/Function_data/lncrna_function_similairty_matrix.mat
+load ./Dataset/Function_data/mirna_function_similairty_matrix.mat
+load ./Dataset/Function_data/invalid_lnc_function.mat
+load ./Dataset/Function_data/invalid_mi_function.mat
+load ./Dataset/miRNA-lnRNA_interaction/interaction.mat
 addpath("./Tools/")
 
 num_Known_Association=length(find(interaction==1));
@@ -31,15 +31,28 @@ Random_order=zeros(num_Known_Association,n_test);
 for cv=1:n_test
     Random_order(:,cv)=crossvalind("KFOLD",num_Known_Association,k_fold);
 end
-parfor cv=1:n_test
+alph=0.001:0.0015:0.02;
+alph=alph';
+alph_list=zeros(n_test,1);
+
+for cv=1:n_test
     if overallauc(cv,1)~=0
         continue;
     end
 
-    POSITION(cv,:)=Method_SP(interaction,Sim1, Sim2, k_fold,Random_order(:,cv),0.018);
-
-	
-	[fpr(cv,:),tpr(cv,:),overallauc(cv,1)] = positiontooverallauc(POSITION(cv,:),interaction,k_fold);	
+    tmp_auc=zeros(length(alph),1);
+    
+    parfor alph_i = 1: length(alph)
+        [tmp_POSITION(alph_i,:)]=Method_SP(interaction,Sim1, Sim2, k_fold,Random_order(:,cv),alph( alph_i ));
+        [tmp_fpr(alph_i,:),tmp_tpr(alph_i,:),tmp_auc(alph_i)] = positiontooverallauc(tmp_POSITION(alph_i,:),interaction,k_fold);
+    end
+    index_tmp_auc=find(tmp_auc == max(tmp_auc));
+    fpr(cv,:)=tmp_fpr(index_tmp_auc(1),:);
+    tpr(cv,:)=tmp_tpr(index_tmp_auc(1),:);
+    overallauc(cv,1) = tmp_auc(index_tmp_auc(1));
+    POSITION(cv,:) = tmp_POSITION(index_tmp_auc(1),:);
+    alph_list(cv)=alph( index_tmp_auc(1) );
+    
 end
 
 Auc_avg=mean(overallauc(:,1));
@@ -47,5 +60,5 @@ AUC_std=std(overallauc(:,1));
 if exist('Results','dir')==0
     mkdir("Results");
 end
-save Results/function_results.mat POSITION Auc_avg AUC_std Random_order fpr tpr
+save ./Results/function_results.mat POSITION overallauc Auc_avg AUC_std Random_order fpr tpr alph_list
 toc
